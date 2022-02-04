805164d13fa0fa0a631b49f27efdc5bc009d371b
jlongo <jeffrey.longo@caci.com>
2022-02-04 12:52:13 -0800

    add stm32l4 lib generation

--- cmake/stm32cubel4.cmake
+++ cmake/stm32cubel4.cmake
@@ -5,20 +5,56 @@ set(HAL_DRIVERS_L4
     spi sram swpmi tim tsc uart usart wwdg
 )
 
-# TODO: fmc/sdmmc/usb requires HAL conf for some reason
 set(LL_DRIVERS_L4
-    adc comp crc crs dac dma dma2d exti gpio i2c lptim lpuart opamp pka pwr 
-    rcc rng rtc spi swpmi tim usart utils
+    adc comp crc crs dac dma dma2d exti fmc gpio i2c lptim lpuart opamp pka pwr 
+    rcc rng rtc sdmmc spi swpmi tim usart usb utils
 )
 
-function(find_stm32cubel4 mcu)
-  # TODO: validate input
+function(generate_stm32cubel4 mcu)
+  set(options)
+  set(args)
+  set(list_args HAL_LIBS LL_LIBS)
+  cmake_parse_arguments(PARSE_ARGV 1 
+    ARG
+    "${options}"
+    "${args}"
+    "${list_args}"
+  )
 
+  foreach(arg IN LISTS ARG_UNPARSED_ARGUMENTS)
+    message(WARNING "Invalid argument: ${arg}")
+  endforeach()
+
+  # validate MCU
   string(TOUPPER ${mcu} mcu_upper)
   string(TOLOWER ${mcu} mcu_lower)
+  string(REGEX MATCH "^STM32([FGHLUW][0-9])([0-9][0-9])$" mcu_match ${mcu_upper})
+  if(NOT mcu_match)
+    message(FATAL_ERROR "Unknown MCU: ${mcu}")
+  endif()
+
+  # validate HAL libraries
+  foreach(lib IN LISTS ARG_HAL_LIBS)
+    if(lib IN_LIST HAL_DRIVERS_L4)
+      message(VERBOSE "Adding HAL library: ${lib}")
+      list(APPEND hal_drivers ${lib})
+    else()
+      message(WARNING "Invalid HAL library: ${lib}")
+    endif()
+  endforeach()
+
+  # validate LL libraries
+  foreach(lib IN LISTS ARG_LL_LIBS)
+    if(lib IN_LIST LL_DRIVERS_L4)
+      message(VERBOSE "Adding LL library: ${lib}")
+      list(APPEND ll_drivers ${lib})
+    else()
+      message(WARNING "Invalid LL library: ${lib}")
+    endif()
+  endforeach()
 
   # build CMSIS
-  add_library(${mcu_lower} STATIC
+  add_library(${mcu_lower} OBJECT
     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/gcc/startup_${mcu_lower}xx.s
     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Source/Templates/system_stm32l4xx.c
   )
@@ -29,20 +65,20 @@ function(find_stm32cubel4 mcu)
   )
 
   # build HAL libraries
-  # foreach(driver IN LISTS HAL_DRIVERS_L4)
-  #   add_library(stm32l4_hal_${driver} STATIC
-  #     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_${driver}.c)
-  #   target_compile_definitions(stm32l4_hal_${driver} PUBLIC -D${mcu_upper}xx -DUSE_HAL_DRIVER)
-  #   target_include_directories(stm32l4_hal_${driver} PUBLIC
-  #     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Core/Include
-  #     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Include
-  #     ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc
-  #   )
-  # endforeach()
+  foreach(driver IN LISTS hal_drivers)
+    add_library(stm32l4_hal_${driver} OBJECT
+      ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_${driver}.c)
+    target_compile_definitions(stm32l4_hal_${driver} PUBLIC -D${mcu_upper}xx -DUSE_HAL_DRIVER)
+    target_include_directories(stm32l4_hal_${driver} PUBLIC
+      ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Core/Include
+      ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/CMSIS/Device/ST/STM32L4xx/Include
+      ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Inc
+    )
+  endforeach()
 
   # build LL libraries
-  foreach(driver IN LISTS LL_DRIVERS_L4)
-    add_library(stm32l4_ll_${driver} STATIC
+  foreach(driver IN LISTS ll_drivers)
+    add_library(stm32l4_ll_${driver} OBJECT
       ${CMAKE_SOURCE_DIR}/STM32CubeL4/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_ll_${driver}.c)
     target_compile_definitions(stm32l4_ll_${driver} PUBLIC -D${mcu_upper}xx -DUSE_FULL_LL_DRIVER)
     target_include_directories(stm32l4_ll_${driver} PUBLIC
